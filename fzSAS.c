662346dd49d8d38d020d06a0acf038d945f4adb6
fatimamnaouer <fatimaezzahra.mnaouer@gmail.com>
2023-09-21 18:36:08 +0100

    sas

--- fzSAS.c
+++ fzSAS.c
@@ -4,24 +4,8 @@
 #include <time.h>
 
 
-int main() {
-    int numero_doperation;
 
-printf("Bienvenue à l'application TO DO\n");
-     printf("1. Ajouter une nouvelle tâche\n");
-     printf("2. Ajouter plusieur nouvelle tâches\n");
-     printf("3. Afficher la liste de toutes les tâche\n");
-     printf("4. Modifier une tâche\n");
-     printf("5. Suprimer une tâche\n");
-     printf("6.Rechercher les tâches\n");
-     printf("7.Statistique\n");
-     printf("8.Sortir");
-     
-
-    return 0;
-}
-
-struct taches
+struct nvtaches// ajouter nouvelle taches
 {
      char titre [100];
      char description [500];
@@ -29,4 +13,68 @@ struct taches
      char deadline;
      int id;
 };
-//declaration des fonctions
+
+// declaration des fonctions:
+
+void choixN1 (){
+    int nombredetache = 0;
+    tache
+    struct nvtaches liste_choix1;
+    printf("entrer le titre de la tache");
+    scanf("%s",tab[])
+
+
+
+
+int main() {
+    int numero_doperation;
+    struct nvtaches.choix
+
+printf("Bienvenue à l'application TO DO\n");
+while(1){
+     printf("1. Ajouter une nouvelle tâche\n");
+     printf("2. Ajouter plusieur nouvelle tâches\n");
+     printf("3. Afficher la liste de toutes les tâche\n");
+     printf("4. Modifier une tâche\n");
+     printf("5. Suprimer une tâche\n");
+     printf("6.Rechercher les tâches\n");
+     printf("7.Statistique\n");
+     printf("8.Sortir\n");
+
+     printf("Choisissez le numero d'operation: ");
+     scanf("%d", &numero_doperation);
+           switch(numero_doperation){
+               case 1: 
+                                //  nouvel tache 
+		 break ;
+	       case 2:
+                               // plusieur nouvelle tâches
+ 	         break;
+	       case 3:
+                              // Affichage des listes
+	         break;
+	       case 4:
+                              // Modifier la tache
+	         break;
+	       case 5:
+                             // Suprimer la tache
+	         break;
+	       case 6:
+                            // Rchercher la taches
+	         break;
+	       case 7:
+                           // Statistique
+	         break;
+	       case 8:
+                           // Quitter
+	         break;
+	default:
+	       printf("Erreur");
+                 break;
+
+           }
+}
+
+    return 0;
+}
+
