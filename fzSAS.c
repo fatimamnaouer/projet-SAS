3a9efa260cef5b86755d130eed275ea3fda80cd3
fatimamnaouer <fatimaezzahra.mnaouer@gmail.com>
2023-09-21 23:06:24 +0200

    modification

--- fzSAS.c
+++ fzSAS.c
@@ -5,30 +5,49 @@
 
 
 
-struct nvtaches// ajouter nouvelle taches
-{
+typedef struct // ajouter nouvelle taches
+{ 
+
      char titre [100];
      char description [500];
      char statut [30];
-     char deadline;
+     char deadline[11];
      int id;
-};
+}tache;
+	int nombredetache = 0;
+	tache tab[100];
 
 // declaration des fonctions:
 
 void choixN1 (){
-    int nombredetache = 0;
-    tache
-    struct nvtaches liste_choix1;
+	int ndestatut;
+
     printf("entrer le titre de la tache");
-    scanf("%s",tab[])
-
-
+    scanf("%s",tab[nombredetache].titre);
+    printf("entrer la description");
+    scanf("%s", tab[nombredetache].description);
+    printf("entrer un deadline");
+    scanf("%s", tab[nombredetache].deadline);
+    printf("entrer le numero 1 pour tache à realiser , numero 2 pour tache en cours , 3 pour tache finalisée"); 
+    scanf("%d", &ndestatut);
+  	        
+		if(ndestatut==1)
+	           strcpy(tab[nombredetache].statut , "à realiser");
+                 else if (ndestatut ==2)
+                    strcpy(tab[nombredetache].statut , "en cours");
+                 else if (ndestatut ==3)
+                    strcpy(tab[nombredetache].statut , "finalisee");
+                 else
+                     printf("veuillez choisir 1 ou 2 ou 3");
+       
+        tab[nombredetache].id=nombredetache+1;
+        nombredetache++;
+}
 
 
 int main() {
     int numero_doperation;
-    struct nvtaches.choix
+
 
 printf("Bienvenue à l'application TO DO\n");
 while(1){
@@ -43,30 +62,30 @@ while(1){
 
      printf("Choisissez le numero d'operation: ");
      scanf("%d", &numero_doperation);
-           switch(numero_doperation){
+       switch(numero_doperation){ 
                case 1: 
-                                //  nouvel tache 
+                    choixN1();            //  nouvel tache 
 		 break ;
 	       case 2:
-                               // plusieur nouvelle tâches
+                     printf("bv choix 2");          // plusieur nouvelle tâches
  	         break;
 	       case 3:
-                              // Affichage des listes
+                     printf("bv choix 3");         // Affichage des listes
 	         break;
 	       case 4:
-                              // Modifier la tache
+                     printf("bv choix 4");         // Modifier la tache
 	         break;
 	       case 5:
-                             // Suprimer la tache
+                     printf("bv choix 5");        // Suprimer la tache
 	         break;
 	       case 6:
-                            // Rchercher la taches
+                     printf("bv choix 6");       // Rchercher la taches
 	         break;
 	       case 7:
-                           // Statistique
+                      printf("bv choix 7");     // Statistique
 	         break;
 	       case 8:
-                           // Quitter
+                      printf("bv choix 8");     // Quitter
 	         break;
 	default:
 	       printf("Erreur");
